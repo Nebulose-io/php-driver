Nebulose-io/php-driver

commit bbfb5118b377a92550339ac798702f91fd1995a9

api: Upgrading to C/C++ driver v2.0.0 while fixing Windows build
This commit upgrade the submodule and code to work with the C/C++
driver v2.0.0 while addressing cross platform build issues on the
Windows platform. Cleanup was also done to remove parent relative
includes for php_cassandra.h.

ext/util/inet.c
@@ -1,4 +1,4 @@
-#include "../php_cassandra.h"
+#include "php_cassandra.h"
 #include <stdlib.h>
 #include "util/inet.h"
 
@@ -9,13 +9,11 @@ extern zend_class_entry* cassandra_ce_InvalidArgumentException;
 #define TOKEN_MAX_LEN    4
 #define IP_MAX_ADDRLEN   50
 #define EXPECTING_TOKEN(expected) \
-  ({ \
     zend_throw_exception_ex(cassandra_ce_InvalidArgumentException, 0 TSRMLS_CC, \
       "Unexpected %s at position %d in address \"%s\", expected " expected, \
       ip_address_describe_token(type), ((int) (in_ptr - in) - 1), in \
     ); \
-    return 0; \
-  })
+    return 0;
 
 enum token_type {
   TOKEN_END = 0,

ext/types/inet.c
@@ -1,4 +1,4 @@
-#include "../php_cassandra.h"
+#include "php_cassandra.h"
 #include "util/inet.h"
 #include "inet.h"
 
@@ -83,11 +83,14 @@ php_cassandra_inet_properties(zval *object TSRMLS_DC)
 static int
 php_cassandra_inet_compare(zval *obj1, zval *obj2 TSRMLS_DC)
 {
+  cassandra_inet* inet1 = NULL;
+  cassandra_inet* inet2 = NULL;
+
   if (Z_OBJCE_P(obj1) != Z_OBJCE_P(obj2))
     return 1; /* different classes */
 
-  cassandra_inet* inet1 = (cassandra_inet*) zend_object_store_get_object(obj1 TSRMLS_CC);
-  cassandra_inet* inet2 = (cassandra_inet*) zend_object_store_get_object(obj2 TSRMLS_CC);
+  inet1 = (cassandra_inet*) zend_object_store_get_object(obj1 TSRMLS_CC);
+  inet2 = (cassandra_inet*) zend_object_store_get_object(obj2 TSRMLS_CC);
 
   if (inet1->inet.address == inet2->inet.address)
     return 0;

ext/exceptions/domain.c
@@ -1,4 +1,4 @@
-#include "../php_cassandra.h"
+#include "php_cassandra.h"
 #include "exception.h"
 
 extern zend_class_entry* spl_ce_DomainException;

ext/exceptions/exception.c
@@ -1,4 +1,4 @@
-#include "../php_cassandra.h"
+#include "php_cassandra.h"
 #include "exception.h"
 
 zend_class_entry *cassandra_ce_Exception = NULL;

ext/exceptions/invalid_argument.c
@@ -1,4 +1,4 @@
-#include "../php_cassandra.h"
+#include "php_cassandra.h"
 #include "exception.h"
 
 extern zend_class_entry* spl_ce_InvalidArgumentException;

ext/exceptions/logic.c
@@ -1,4 +1,4 @@
-#include "../php_cassandra.h"
+#include "php_cassandra.h"
 #include "exception.h"
 
 extern zend_class_entry* spl_ce_LogicException;

ext/exceptions/runtime.c
@@ -1,4 +1,4 @@
-#include "../php_cassandra.h"
+#include "php_cassandra.h"
 #include "exception.h"
 
 extern zend_class_entry* spl_ce_RuntimeException;

ext/exceptions/server.c
@@ -1,4 +1,4 @@
-#include "../php_cassandra.h"
+#include "php_cassandra.h"
 #include "exception.h"
 
 extern zend_class_entry* cassandra_ce_RuntimeException;

commit a6b1e0b7064fb6fe4c47355b18ead5a4dca3d2fe

Fixes macro expansion bug in util/inet.c
Fixes bug introduced by https://github.com/nano-interactive/cassandra-php-driver/commit/bbfb5118b377a92550339ac798702f91fd1995a9

ext/util/inet.c
@@ -336,8 +336,9 @@ php_driver_parse_ip_address(char *in, CassInet *inet TSRMLS_DC)
     if (state == STATE_END) {
       if (type == TOKEN_END)
         break;
-      else
+      else {
         EXPECTING_TOKEN("the end of address");
+      }
     }
   }
 